5bdc42800805da20d7dd53f3ed0e72e8039f04ec
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2016-07-17 21:10:55 +0300

    Добавлен #ifdef для отладки

--- main.cpp
+++ main.cpp
@@ -2,12 +2,18 @@
 #include "http_server.h"
 #include "daemonize.h"
 
+//#define LOCALE_DEBUG 1
+
 int main(int argc, char *argv[])
 {
+#ifdef LOCALE_DEBUG
+  std::unique_ptr<args_parser> parser(new args_parser);
+  server_params_t params = parser->get_params();
+#else
   std::unique_ptr<daemonize> daem(new daemonize);
   std::unique_ptr<args_parser> parser(new args_parser);
   server_params_t params = parser->parse_arguments(argc, argv);
-  //server_params_t params = parser->get_params();
+#endif
   std::unique_ptr<http_server> server(new http_server(params));
   return 0;
 }
