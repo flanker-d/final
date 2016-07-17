1a16380f76ee83cd18732953ddf708bc63b37f11
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2016-07-17 22:59:30 +0300

    Изменён формат ответа

--- http_parser.cpp
+++ http_parser.cpp
@@ -49,21 +49,21 @@ std::string http_parser::get_response()
     read_input.read(input_file.get(), size);
     read_input.close();
 
-    response_.append("HTTP/1.0 200 OK\r\n");
-    response_.append("Content-Type: text/html\r\n");
-    response_.append("Content-length:" + std::to_string(size) +"\r\n");
-    response_.append("Connection: close\r\n\r\n");
+    response_.append("HTTP/1.0 200 OK\r\n\r\n");
+    //response_.append("Content-Type: text/html\r\n");
+    //response_.append("Content-length:" + std::to_string(size) +"\r\n");
+    //response_.append("Connection: close\r\n\r\n");
     response_.append(input_file.get());
   }
   else
   {
-    std::string page404("<html><head><title>404</title></head><body>404 Not Found</body></html>\r\n");
-
+    //std::string page404("<html><head><title>404</title></head><body>404 Not Found</body></html>\r\n");
     response_.append("HTTP/1.0 404 Not Found\r\n");
-    response_.append("Content-Type: text/html\r\n");
-    response_.append("Content-length:" + std::to_string(page404.size()) +"\r\n");
-    response_.append("Connection: close\r\n\r\n");
-    response_.append(page404);
+    response_.append("Content-length: 0\r\n");
+    response_.append("Content-Type: text/html\r\n\r\n");
+    // + std::to_string(page404.size()) +"\r\n");
+    //response_.append("Connection: close\r\n\r\n");
+    //response_.append(page404);
   }
 
   return response_;
